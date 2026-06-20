1a8edaf6543b1cc21021826b86aecab854617ea0
Saripudi Jayalakshmi <your-email@example.com>
2026-06-20 13:46:39 +0530

    fix: update vite config for production build

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,4 +3,11 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    chunkSizeWarningLimit: 1000,
+  },
+  define: {
+    'process.env': {}
+  }
 })
